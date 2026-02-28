dd9cecbcfb855ebeafc91f8b276528193a09be58
Kenta Ozawa <136443889+Gingnose@users.noreply.github.com>
2026-02-28 17:00:01 +0900

    fix: add va opt instead of ## for comma eating in log.h

--- engine/core/log.h
+++ engine/core/log.h
@@ -14,25 +14,25 @@
 
 #define ORO_LOG_TRACE(fmt, ...) \
     do { if (ORO_LOG_LEVEL_TRACE >= ORO_LOG_MIN_LEVEL) \
-        fprintf(stderr, "[TRACE] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+        fprintf(stderr, "[TRACE] %s:%d: " fmt "\n", __FILE__, __LINE__, __VA_OPT__(, ) __VA_ARGS__); \
     } while (0)
 #define ORO_LOG_DEBUG(fmt, ...) \
     do { if (ORO_LOG_LEVEL_DEBUG >= ORO_LOG_MIN_LEVEL) \
-        fprintf(stderr, "[DEBUG] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+        fprintf(stderr, "[DEBUG] %s:%d: " fmt "\n", __FILE__, __LINE__, __VA_OPT__(, ) __VA_ARGS__); \
     } while (0)
 #define ORO_LOG_INFO(fmt, ...) \
     do { if (ORO_LOG_LEVEL_INFO >= ORO_LOG_MIN_LEVEL) \
-        fprintf(stderr, "[INFO] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+        fprintf(stderr, "[INFO] %s:%d: " fmt "\n", __FILE__, __LINE__, __VA_OPT__(, ) __VA_ARGS__); \
     } while (0)
 #define ORO_LOG_WARN(fmt, ...) \
     do { if (ORO_LOG_LEVEL_WARN >= ORO_LOG_MIN_LEVEL) \
-        fprintf(stderr, "[WARN] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+        fprintf(stderr, "[WARN] %s:%d: " fmt "\n", __FILE__, __LINE__, __VA_OPT__(, ) __VA_ARGS__); \
     } while (0)
 #define ORO_LOG_ERROR(fmt, ...) \
     do { if (ORO_LOG_LEVEL_ERROR >= ORO_LOG_MIN_LEVEL) \
-        fprintf(stderr, "[ERROR] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+        fprintf(stderr, "[ERROR] %s:%d: " fmt "\n", __FILE__, __LINE__, __VA_OPT__(, ) __VA_ARGS__); \
     } while (0)
 #define ORO_LOG_FATAL(fmt, ...) \
     do { if (ORO_LOG_LEVEL_FATAL >= ORO_LOG_MIN_LEVEL) \
-        fprintf(stderr, "[FATAL] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+        fprintf(stderr, "[FATAL] %s:%d: " fmt "\n", __FILE__, __LINE__, __VA_OPT__(, ) __VA_ARGS__); \
     } while (0)
