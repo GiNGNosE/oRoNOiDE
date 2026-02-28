6fb8c4cbaa80c5129c7305ac305bee4d9b996c65
Kenta Ozawa <136443889+Gingnose@users.noreply.github.com>
2026-02-28 18:25:43 +0900

    feat: implement context cpp

--- engine/rhi/vulkan/context.cpp
+++ engine/rhi/vulkan/context.cpp
@@ -1,1 +1,41 @@
 #include "rhi/vulkan/context.h"
+#include "core/log.h"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+#include <vector>
+
+bool VulkanContext::createInstance() {
+    VkApplicationInfo appInfo = {};
+    appInfo.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName   = "oRoNOiDE";
+    appInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+    appInfo.engineName         = "oRoNOiDE Engine";
+    appInfo.engineVersion      = VK_MAKE_VERSION(0, 1, 0);
+    appInfo.apiVersion         = VK_API_VERSION_1_3;
+
+    uint32_t sdlExtCount = 0;
+    const char* const* sdlExts = SDL_Vulkan_GetInstanceExtensions(&sdlExtCount);
+    if (!sdlExts) {
+        ORO_LOG_ERROR("Failed to get Vulkan instance extensions from SDL");
+        return false;
+    }
+
+    std::vector<const char*> extensions(sdlExts, sdlExts + sdlExtCount);
+    extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType                   = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo        = &appInfo;
+    createInfo.enabledExtensionCount   = static_cast<uint32_t>(extensions.size());
+    createInfo.ppEnabledExtensionNames = extensions.data();
+    createInfo.flags                   = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &m_instance);
+    if (result != VK_SUCCESS) {
+        ORO_LOG_ERROR("Failed to create Vulkan instance: %d", result);
+        return false;
+    }
+
+    ORO_LOG_INFO("Vulkan instance created successfully");
+    return true;
+}
--- engine/rhi/vulkan/context.h
+++ engine/rhi/vulkan/context.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <vulkan/vulkan.h>
+
+struct SDL_Window;
+
+class VulkanContext {
+public:
+    bool init(SDL_Window* window);
+    void shutdown();
+
+    VkInstance       instance()       const { return m_instance; }
+    VkPhysicalDevice physicalDevice() const { return m_physicalDevice; }
+    VkDevice         device()         const { return m_device; }
+    VkQueue          graphicsQueue()  const { return m_graphicsQueue; }
+    VkSurfaceKHR     surface()        const { return m_surface; }
+    uint32_t         graphicsFamily() const { return m_graphicsFamily; }
+
+private:
+    bool createInstance();
+    bool createSurface(SDL_Window* window);
+    bool pickPhysicalDevice();
+    bool createLogicalDevice();
+
+    VkInstance               m_instance       = VK_NULL_HANDLE;
+    VkDebugUtilsMessengerEXT m_debugMessenger = VK_NULL_HANDLE;
+    VkSurfaceKHR             m_surface        = VK_NULL_HANDLE;
+    VkPhysicalDevice         m_physicalDevice = VK_NULL_HANDLE;
+    VkDevice                 m_device         = VK_NULL_HANDLE;
+    VkQueue                  m_graphicsQueue  = VK_NULL_HANDLE;
+    uint32_t                 m_graphicsFamily = 0;
+};
